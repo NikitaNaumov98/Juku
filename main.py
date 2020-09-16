2ac5b6cb49a765fe4d1c0151d1e2dc686f8dcfef
Nikita Naumov <niknau2009@gmail.com>
2020-09-16 22:06:53 +0300

    added timeout and stop command, moved most commands into separate function

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import serial
 
-devname = "/dev/ttyUSB0"
-
-ser = serial.Serial(devname)
-
-while True:
-    userc = input("Type command:")
+def communication(userc):
     if userc == "gs":
         ser.write(userc + "\n")
         answer = ser.readline()
@@ -24,4 +19,21 @@ while True:
         if len(poslist) == 2:
             ser.write("sv:" + poslist[1] + "\n")
     elif userc.startswith("rf:"):
-        ser.write(userc + "\n")
+        rflist = userc.split(":")
+        if len(rflist) == 2:
+            ser.write(rflist[1] + "\n")
+
+#proovida kasutada serial.tools.list_ports
+#muuta timeout kui on vaja
+ser = serial.Serial("/dev/ttyUSB0", timeout=2, write_timeout=2)
+
+while True:
+    userc = input("Type command:")
+    if ser.in_writing > 0:
+        inputbuff = ser.read()
+        inputbuff = inputbuff.rstrip("\n")
+        print(printbuff)
+    if userc == "stop":
+        ser.close()
+        break
+    communication(userc)
